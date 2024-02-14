8b233525f8d36bccead178d496204c64f42bc8b4
rajasree9 <rt484@njit.edu>
2024-02-14 13:16:57 -0500

    changed the code

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,32 +1,37 @@
-'''Testing Operations'''
+"""
+This module contains tests for the calculator operations module.
+
+The tests are designed to verify the correctness of the arithmetic operations
+(addition, subtraction, multiplication, division) implemented in the calculator.operations module.
+"""
+
 from decimal import Decimal
 import pytest
 from calculator.calculation import Calculation
 from calculator.operations import add, subtract, multiply, divide
 
-
 def test_operation_add():
-    '''Testing the addition operation'''
+    """Test the addition operation"""
     calculation = Calculation(Decimal('10'), Decimal('5'), add)
-    assert calculation.perform() == Decimal('15'), "Add operation failed"
+    assert calculation.perform() == Decimal('15')
 
 def test_operation_subtract():
-    '''Testing the subtract operation'''
+    """Test the subtraction operation"""
     calculation = Calculation(Decimal('10'), Decimal('5'), subtract)
-    assert calculation.perform() == Decimal('5'), "Subtract operation failed"
+    assert calculation.perform() == Decimal('5')
 
 def test_operation_multiply():
-    '''Testing the multiply operation'''
+    """Test the multiplication operation"""
     calculation = Calculation(Decimal('10'), Decimal('5'), multiply)
-    assert calculation.perform() == Decimal('50'), "Multiply operation failed"
+    assert calculation.perform() == Decimal('50')
 
 def test_operation_divide():
-    '''Testing the divide operation'''
+    """Test the division operation"""
     calculation = Calculation(Decimal('10'), Decimal('5'), divide)
-    assert calculation.perform() == Decimal('2'), "Divide operation failed"
+    assert calculation.perform() == Decimal('2')
 
 def test_divide_by_zero():
-    '''Testing the divide by zero exception'''
+    """Test the divide by zero exception"""
     with pytest.raises(ValueError, match="Cannot divide by zero"):
         calculation = Calculation(Decimal('10'), Decimal('0'), divide)
         calculation.perform()
