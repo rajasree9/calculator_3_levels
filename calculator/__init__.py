f17d77c38ce1b55b8e789159750b08ad0392b202
rajasree9 <rt484@njit.edu>
2024-02-14 14:15:07 -0500

    added test calculator

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,21 +1,5 @@
-from calculator.operations import add, subtract, multiply, divide
+def add(a,b):
+    return a + b
 
-class SimpleCalculator:
-    @staticmethod
-    def add(a, b):
-        return add(a, b)
-
-    @staticmethod
-    def subtract(a, b):
-        return subtract(a, b)
-
-    @staticmethod
-    def multiply(a, b):
-        return multiply(a, b)
-
-    @staticmethod
-    def divide(a, b):
-        if b != 0:
-            return divide(a, b)
-        else:
-            return "Cannot divide by zero"
+def subtract(a,b):
+    return a - b
